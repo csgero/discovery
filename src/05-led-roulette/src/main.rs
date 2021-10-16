d4560f3e1e8c19e2d08b69b4234e74d80360ba1a
Csaba Gero <csgero@gmail.com>
2021-10-16 21:28:29 +0200

    fix solution for 05-led-roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -8,23 +8,19 @@ use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
     
-    let half_period = 50_u16;
+    let period = 50_u16;
 
     let mut curr: usize = 0;
-    let mut prev: usize = 7;
-    let mut next: usize = 1;
 
     leds[0].on().ok();
     leds[1].on().ok();
     
     loop {
-        delay.delay_ms(half_period);
-        leds[prev].off().ok();
-        delay.delay_ms(half_period);
-        leds[next].on().ok(); 
-        
-        prev = if curr == 0 {7} else {curr - 1};
-        next = if curr == 7 {0} else {curr + 1};
-        curr = next;
+        let next = (curr + 1) % 8;
+        leds[next].on().ok();
+        delay.delay_ms(period);
+        leds[curr].off().ok();
+        delay.delay_ms(period);
+        curr = next; 
     }
 }
