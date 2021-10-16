573df873553a7f7177ddca8012dd0884daaff983
Csaba Gero <csgero@gmail.com>
2021-10-16 21:04:06 +0200

    solve 05-led-roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,29 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
+    
+    let half_period = 50_u16;
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut curr: usize = 0;
+    let mut prev: usize = 7;
+    let mut next: usize = 1;
+
+    leds[0].on().ok();
+    leds[1].on().ok();
+    
+    loop {
+        delay.delay_ms(half_period);
+        leds[prev].off().ok();
+        delay.delay_ms(half_period);
+        leds[next].on().ok(); 
+        
+        prev = if curr == 0 {7} else {curr - 1};
+        next = if curr == 7 {0} else {curr + 1};
+        curr = next;
+    }
 }
