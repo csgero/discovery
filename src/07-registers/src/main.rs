69cfa6c95a34547b0e92f24a02a3997c330c3078
Csaba Gero <csgero@gmail.com>
2021-10-17 20:11:32 +0200

    07-registers: type safe manipulation

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -3,48 +3,23 @@
 
 use core::ptr;
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln, ITM};
-
-// Print the current contents of odr
-fn iprint_odr(itm: &mut ITM) {
-    const GPIOE_ODR: u32 = 0x4800_1014;
-
-    unsafe {
-        iprintln!(
-            &mut itm.stim[0],
-            "ODR = {:#016b}",
-            ptr::read_volatile(GPIOE_ODR as *const u16)
-        );
-    }
-}
+use aux7::{entry, iprint, iprintln, ITM, RegisterBlock};
 
 #[entry]
 fn main() -> ! {
-    let mut itm= aux7::init().0;
+    let gpioe= aux7::init().1;
 
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+    // Turn on the North LED
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
 
-        // Print the initial contents of ODR
-        iprint_odr(&mut itm);
+    // Turn on the East LED
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
 
-        // Turn on the "North" LED (red)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
-        iprint_odr(&mut itm);
+    // Turn off the North LED
+    gpioe.bsrr.write(|w| w.br9().set_bit());
 
-        // Turn on the "East" LED (green)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
-        iprint_odr(&mut itm);
-
-        // Turn off the "North" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
-        iprint_odr(&mut itm);
-
-        // Turn off the "East" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
-        iprint_odr(&mut itm);
-    }
+    // Turn off the East LED
+    gpioe.bsrr.write(|w| w.br11().set_bit());
 
     loop {}
 }
