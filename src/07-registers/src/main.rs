d6706311690b1918c17cf506f5f8a28eb7e6e5ac
Csaba Gero <csgero@gmail.com>
2021-10-17 19:58:14 +0200

    07-registers: read register state

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -3,27 +3,47 @@
 
 use core::ptr;
 #[allow(unused_imports)]
-use aux7::{entry, iprint, iprintln};
+use aux7::{entry, iprint, iprintln, ITM};
+
+// Print the current contents of odr
+fn iprint_odr(itm: &mut ITM) {
+    const GPIOE_ODR: u32 = 0x4800_1014;
+
+    unsafe {
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = {:#016b}",
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let mut itm= aux7::init().0;
 
     unsafe {
         // A magic address!
         const GPIOE_BSRR: u32 = 0x48001018;
 
+        // Print the initial contents of ODR
+        iprint_odr(&mut itm);
+
         // Turn on the "North" LED (red)
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+        iprint_odr(&mut itm);
 
         // Turn on the "East" LED (green)
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+        iprint_odr(&mut itm);
 
         // Turn off the "North" LED
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+        iprint_odr(&mut itm);
 
         // Turn off the "East" LED
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+        iprint_odr(&mut itm);
     }
 
     loop {}
