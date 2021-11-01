3c36b743ec666ede15cf564be10f5b7ce576faaa
Csaba Gero <csgero@gmail.com>
2021-11-01 17:49:25 +0100

    11-usart: reverse a string

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -5,6 +5,7 @@ use core::fmt::{self, Write};
 
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln, usart1};
+use heapless::Vec;
 
 macro_rules! uprint {
     ($serial:expr, $($arg:tt)*) => {
@@ -22,7 +23,7 @@ macro_rules! uprintln {
 }
 
 struct SerialPort<'a> {
-    usart1: &'a mut usart1::RegisterBlock,
+    usart1: &'a usart1::RegisterBlock,
 }
 
 impl fmt::Write for SerialPort<'_> {
@@ -34,13 +35,13 @@ impl fmt::Write for SerialPort<'_> {
     }
 }
 
-fn send_byte(usart1: &usart1::RegisterBlock, b: u8)  {
-    while usart1.isr.read().txe().bit_is_clear() {};
+fn send_byte(usart1: &usart1::RegisterBlock, b: u8) {
+    while usart1.isr.read().txe().bit_is_clear() {}
     usart1.tdr.write(|w| w.tdr().bits(u16::from(b)));
 }
 
 fn receive_byte(usart1: &usart1::RegisterBlock) -> u8 {
-    while usart1.isr.read().rxne().bit_is_clear() {};
+    while usart1.isr.read().rxne().bit_is_clear() {}
     usart1.rdr.read().rdr().bits() as u8
 }
 
@@ -49,10 +50,28 @@ fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
     let mut serial = SerialPort { usart1 };
 
-    uprintln!(serial, "Echoing...");
+    uprintln!(serial, "Reversing...");
+
+    let mut buffer: Vec<u8, 32> = Vec::new();
 
     loop {
-        let byte = receive_byte(usart1);
-        send_byte(usart1, byte);
+        buffer.clear();
+
+        loop {
+            let byte_in = receive_byte(usart1);
+            if byte_in != b'\r' {
+                if buffer.push(byte_in).is_err() {
+                    uprintln!(serial, "Error: buffer full");
+                    break;
+                }
+            } else {
+                while let Some(byte_out) = buffer.pop() {
+                    send_byte(usart1, byte_out);
+                }
+                send_byte(usart1, b'\n');
+                send_byte(usart1, b'\r');
+                break;
+            }
+        }
     }
 }
