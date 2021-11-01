55d5dbf4b65fea80b768d58ee3123b9445534757
Csaba Gero <csgero@gmail.com>
2021-11-01 20:02:42 +0100

    14-i2c: read magnetometer WHO_AM_I register

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -10,7 +10,7 @@ const MAGNETOMETER: u16 = 0b0011_1100;
 
 // Addresses of the magnetometer's registers
 const OUT_X_H_M: u8 = 0x03;
-const IRA_REG_M: u8 = 0x0A;
+const WHO_AM_I: u8 = 0x4F;
 
 #[entry]
 fn main() -> ! {
@@ -18,28 +18,37 @@ fn main() -> ! {
 
     // Stage 1: Send the address of the register we want to read to the
     // magnetometer
-    {
-        // TODO Broadcast START
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Write
+    i2c1.cr2.write(|w| {
+        w.add10().clear_bit();
+        w.rd_wrn().write();
+        w.sadd().bits(MAGNETOMETER);
+        w.nbytes().bits(1);
+        w.autoend().clear_bit();
+        w.start().start()
+    });
 
-        // TODO Send the address of the register that we want to read: IRA_REG_M
-    }
+    while i2c1.isr.read().txis().bit_is_clear() {}
+    i2c1.txdr.write(|w| w.txdata().bits(WHO_AM_I));
+
+    while i2c1.isr.read().tc().bit_is_clear() {}
 
     // Stage 2: Receive the contents of the register we asked for
     let byte = {
-        // TODO Broadcast RESTART
+        i2c1.cr2.write(|w| {
+            w.rd_wrn().read();
+            w.sadd().bits(MAGNETOMETER);
+            w.nbytes().bits(1);
+            w.autoend().set_bit();
+            w.start().start()
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Read
-
-        // TODO Receive the contents of the register
-
-        // TODO Broadcast STOP
-        0
+        while i2c1.isr.read().rxne().bit_is_clear() {}
+        i2c1.rxdr.read().bits()
     };
 
     // Expected output: 0x0A - 0b01001000
-    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", IRA_REG_M, byte);
+    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", WHO_AM_I, byte);
 
     loop {}
 }
