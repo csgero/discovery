63c768eca583e8951a386abe2d7c3aa70a5d8872
Csaba Gero <csgero@gmail.com>
2021-11-06 19:04:23 +0100

    14-i2c: read magnetometer output

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -1,4 +1,4 @@
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
@@ -9,46 +9,59 @@ use aux14::{entry, iprint, iprintln, prelude::*};
 const MAGNETOMETER: u16 = 0b0011_1100;
 
 // Addresses of the magnetometer's registers
-const OUT_X_H_M: u8 = 0x03;
-const WHO_AM_I: u8 = 0x4F;
+const OUT_X_L_M: u8 = 0xE8;
+const CFG_REG_A_M: u8 = 0x60;
 
 #[entry]
 fn main() -> ! {
-    let (i2c1, _delay, mut itm) = aux14::init();
-
-    // Stage 1: Send the address of the register we want to read to the
-    // magnetometer
+    let (i2c1, mut delay, mut itm) = aux14::init();
 
     i2c1.cr2.write(|w| {
         w.add10().clear_bit();
         w.rd_wrn().write();
         w.sadd().bits(MAGNETOMETER);
-        w.nbytes().bits(1);
-        w.autoend().clear_bit();
+        w.nbytes().bits(2);
+        w.autoend().set_bit();
         w.start().start()
     });
 
+    // configure magnetometer for continuous output
     while i2c1.isr.read().txis().bit_is_clear() {}
-    i2c1.txdr.write(|w| w.txdata().bits(WHO_AM_I));
+    i2c1.txdr.write(|w| w.txdata().bits(CFG_REG_A_M));
+    while i2c1.isr.read().txis().bit_is_clear() {}
+    i2c1.txdr.write(|w| w.txdata().bits(0x0));
 
-    while i2c1.isr.read().tc().bit_is_clear() {}
+    loop {
+        i2c1.cr2.write(|w| {
+            w.rd_wrn().write();
+            w.sadd().bits(MAGNETOMETER);
+            w.nbytes().bits(1);
+            w.autoend().clear_bit();
+            w.start().start()
+        });
 
-    // Stage 2: Receive the contents of the register we asked for
-    let byte = {
+        while i2c1.isr.read().txis().bit_is_clear() {}
+        i2c1.txdr.write(|w| w.txdata().bits(OUT_X_L_M));
+
+        while i2c1.isr.read().tc().bit_is_clear() {}
+
         i2c1.cr2.write(|w| {
             w.rd_wrn().read();
             w.sadd().bits(MAGNETOMETER);
-            w.nbytes().bits(1);
+            w.nbytes().bits(6);
             w.autoend().set_bit();
             w.start().start()
         });
 
-        while i2c1.isr.read().rxne().bit_is_clear() {}
-        i2c1.rxdr.read().bits()
-    };
+        let mut buffer = [0i16; 3];
+        let buffer_u8 = unsafe { buffer.align_to_mut::<u8>().1 };
+        for byte in buffer_u8 {
+            while i2c1.isr.read().rxne().bit_is_clear() {}
+            *byte = i2c1.rxdr.read().rxdata().bits()
+        }
 
-    // Expected output: 0x0A - 0b01001000
-    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", WHO_AM_I, byte);
+        iprintln!(&mut itm.stim[0], "{:?}",buffer);
 
-    loop {}
+        delay.delay_ms(1_000_u16);
+    }
 }
